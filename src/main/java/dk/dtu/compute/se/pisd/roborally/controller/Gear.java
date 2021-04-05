be7d2b4fec95a68a6fd026ea139831abd1013478
Zahed <zahedm45@gmail.com>
2021-04-05 19:20:29 +0200

    gear class added

---
+++ src/main/java/dk/dtu/compute/se/pisd/roborally/controller/Gear.java
@@ -0,0 +1,12 @@
+package dk.dtu.compute.se.pisd.roborally.controller;
+
+import dk.dtu.compute.se.pisd.roborally.model.Space;
+
+public class Gear extends FieldAction{
+
+
+    @Override
+    public boolean doAction(GameController gameController, Space space) {
+        return false;
+    }
+}
